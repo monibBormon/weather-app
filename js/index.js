aa3ef015d787e76341f78ffdbdb3e65b79942cbe
monibBormon <monibbormon14@gmail.com>
2021-09-05 23:48:04 +0600

    add more functionality

--- js/index.js
+++ js/index.js
@@ -26,14 +26,17 @@ const displayWeather = temparature => {
     console.log(temparature);
     const container = document.getElementById('container');
     container.textContent = '';
-    const sunriseTime = convertUnixTimeToLocal(temparature.sys.sunrise)
+    const localDate = convertUnixTimeToLocal(temparature.dt);
+    const sunriseTime = convertUnixTimeToLocal(temparature.sys.sunrise);
+    const sunsetTime = convertUnixTimeToLocal(temparature.sys.sunset);
     const div = document.createElement('div')
     div.innerHTML = `
+        <h4 class="fs-2">${temparature.name}, ${temparature.sys.country}</h4>
+        <h6>${localDate.fullDate}</h6>
         <img src="http://openweathermap.org/img/wn/${temparature.weather[0].icon}@2x.png" alt="">
-        <h4 class="fs-2">${temparature.name}, ${temparature.sys.country}</span></h4>
         <h5 class="fs-1">${temparature.main.temp} &deg;C</h5>
         <h5>${temparature.weather[0].main}</h5>
-        <h5>Sunrise: ${sunriseTime.time12h}</h5>
+        <h5><span class="me-3">Sunrise: ${sunriseTime.time12h}</span> & <span class="ms-3">Sunset: ${sunsetTime.time12h}</span></h5>
     `;
     container.appendChild(div)
 }
